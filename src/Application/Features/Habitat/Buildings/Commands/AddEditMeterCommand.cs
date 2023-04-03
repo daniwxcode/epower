f6e7339eaa03f82f9e6add68357cb22040bc04de
Daniel WOAGOU <pwoagou@otr.tg>
2023-04-03 09:13:15 +0000

    integration des validateurs

---
+++ src/Application/Features/Habitat/Buildings/Commands/AddEditMeterCommand.cs
@@ -0,0 +1,39 @@
+﻿using BlazorHero.CleanArchitecture.Shared.Wrapper;
+
+using FluentValidation;
+
+using MediatR;
+
+using Microsoft.Extensions.Localization;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Application.Features.Habitat.Buildings.Commands;
+
+public class AddEditMeterCommand : IRequest<Result<int>>
+{
+    public int Id { get; set; } = 0;
+    public string SerialNumber { get; set; }
+    public string Code { get; set; }
+}
+
+public class AddEditMeterCommandValidator : AbstractValidator<AddEditMeterCommand>
+{
+    public AddEditMeterCommandValidator(IStringLocalizer<AddEditMeterCommandValidator> localizer)
+    {
+        RuleFor(request => request.SerialNumber)
+            .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage(x => localizer["Serial Number is required!"]);
+        RuleFor(request => request.Code)
+            .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage(x => localizer["Code is required!"]);
+    }
+}
+
+internal class AddEditMeterCommandHandler : IRequestHandler<AddEditMeterCommand, Result<int>>
+{
+    public Task<Result<int>> Handle(AddEditMeterCommand request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Application/Features/Habitat/Buildings/Commands/AddStoreCommand.cs
@@ -0,0 +1,40 @@
+﻿using BlazorHero.CleanArchitecture.Shared.Wrapper;
+
+using FluentValidation;
+using MediatR;
+
+using Microsoft.Extensions.Localization;
+
+using System.ComponentModel;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Application.Features.Habitat.Buildings.Commands;
+
+public class AddEditStoreCommand : IRequest<Result<int>>
+{
+    public int Id { get; set; } = 0;
+    public int BuildingId { get; set; }        
+    public string Name { get; set; }
+    public string SerialNumber { get; set; }
+    public string Code { get; set; }
+}
+public class AddEditStoreCommandValidator : AbstractValidator<AddEditStoreCommand>
+{
+    public AddEditStoreCommandValidator(IStringLocalizer<AddEditStoreCommandValidator> localizer)
+    {
+        RuleFor(request => request.SerialNumber)
+            .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage(x => localizer["Serial Number is required!"]);
+        RuleFor(request => request.BuildingId).Must(
+            x => x > 0).WithMessage(x=>localizer["L'Immeuble est requis"]);
+        RuleFor(request => request.Code)
+            .Must(x => !string.IsNullOrWhiteSpace(x)).WithMessage(x => localizer["Code is required!"]);
+    }
+}
+internal class AddEditStoreCommandHandler: IRequestHandler<AddEditStoreCommand,Result<int>>
+{
+    public Task<Result<int>> Handle(AddEditStoreCommand request, CancellationToken cancellationToken)
+    {
+        throw new System.NotImplementedException();
+    }
+}
