1d30b0543fd5165f127a64d0baa2b170d379a27e
Daniel WOAGOU <pwoagou@otr.tg>
2023-05-05 15:51:08 +0000

    Adding Db Context

---
+++ src/Infrastructure/Contexts/HGDbContext.cs
@@ -0,0 +1,27 @@
+﻿using BlazorHero.CleanArchitecture.Domain.Entities.Bail;
+using BlazorHero.CleanArchitecture.Domain.Entities.Catalog;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Infrastructure.Contexts
+{
+    public partial class BlazorHeroContext
+    {
+        public DbSet<Building> Buildings { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<ShopTenant> ShopTenants { get; set; }
+        public DbSet<Meter> Meters { get; set; }
+        public DbSet<InternalMeter> InternalMeters { get; set; }
+        public DbSet<Payment> Payments { get; set; }
+        public DbSet<InternalPayement> InternalPayements { get; set; }
+        public DbSet<ExternalPayement> ExternalPayements { get; set; }
+        public DbSet<RentalAgreement> RentalAgreements { get; set; }
+        
+
+    }
+}
