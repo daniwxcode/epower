11dd6de55aadbd8279050324da265511280bcecb
Daniel WOAGOU <pwoagou@otr.tg>
2023-05-08 19:53:19 +0000

    merging all in building Controller

---
+++ src/Application/Features/Habitat/Buildings/Queries/GetMeterByIdRequest.cs
@@ -0,0 +1,36 @@
+﻿using BlazorHero.CleanArchitecture.Application.Features.Habitat.Buildings.DTO;
+using BlazorHero.CleanArchitecture.Application.Interfaces.Repositories;
+using BlazorHero.CleanArchitecture.Domain.Entities.Bail;
+using BlazorHero.CleanArchitecture.Shared.Wrapper;
+
+using MediatR;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Application.Features.Habitat.Buildings.Queries
+{
+    public class GetMeterByIdRequest : IRequest<Result<MeterResponseBase>>
+    {
+        public int Id { get; set; }
+        public GetMeterByIdRequest(int id)
+        {
+            Id= id;
+        }
+    }
+    internal class GetMeterByIdRequestHandler : IRequestHandler<GetMeterByIdRequest, Result<MeterResponseBase>>
+    {
+        private IUnitOfWork<int> _unitOfWork;
+        public async Task<Result<MeterResponseBase>> Handle(GetMeterByIdRequest request, CancellationToken cancellationToken)
+        {
+            var dbItem = await _unitOfWork.Repository<Meter>().GetByIdAsync(request.Id);
+            if (dbItem == null)
+                return await Result<MeterResponseBase>.FailAsync("Compteur Inexistant");
+            return await Result<MeterResponseBase>.SuccessAsync(dbItem.GetMeterResponse());            
+        }
+    }
+}
--- src/Server/Controllers/v1/Bail/BuildingController.cs
+++ src/Server/Controllers/v1/Bail/BuildingController.cs
@@ -1,6 +1,7 @@
 ﻿using BlazorHero.CleanArchitecture.Application.Features.Brands.Queries.GetAll;
 using BlazorHero.CleanArchitecture.Application.Features.Habitat.Buildings.Commands;
 using BlazorHero.CleanArchitecture.Application.Features.Habitat.Buildings.Queries;
+using BlazorHero.CleanArchitecture.Application.Features.Habitat.Meters.Queries;
 
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
@@ -17,6 +18,40 @@ namespace BlazorHero.CleanArchitecture.Server.Controllers.v1.Bail
         }
         [HttpPost]
         public async Task<IActionResult> AddBuilding(AddEditBuildingCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+        [HttpGet("all-meters")]
+        public async Task<IActionResult> GetAllMeters()
+        {
+            var data = await _mediator.Send(new GetAllMetersRequest());
+            return Ok(data);
+        }
+        [HttpGet("building-meters//{id}")]
+        public async Task<IActionResult> GetAllByBuilding(int id)
+        {
+            var data = await _mediator.Send(new GetAllMetersByBuildingIdRequest(id));
+            return Ok(data);
+        }
+        [HttpGet("meters/{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var data = await _mediator.Send(new GetMeterByIdRequest(id));
+            return Ok(data);
+        }
+        [HttpPost("add-meter")]
+        public async Task<IActionResult> AddStore(AddEditMeterCommand command)
+        {
+            return Ok(await _mediator.Send(command));
+        }
+        [HttpGet("stores")]
+        public async Task<IActionResult> GetAll()
+        {
+            var data = await _mediator.Send(new GetAllShopRequest());
+            return Ok(data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddStore(AddEditStoreCommand command)
         {
             return Ok(await _mediator.Send(command));
         }
