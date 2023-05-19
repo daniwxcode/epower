91eede30b9cd4146aa2d4ed2af6c927193d24232
Daniel WOAGOU <pwoagou@otr.tg>
2023-05-19 09:12:20 +0000

    strart preparing POS

--- src/Client/Pages/CashPower/Pos.razor.cs
+++ src/Client/Pages/CashPower/Pos.razor.cs
@@ -1,6 +1,20 @@
-﻿namespace BlazorHero.CleanArchitecture.Client.Pages.CashPower
+﻿using BlazorHero.CleanArchitecture.Application.Features.Habitat.Buildings.Commands;
+
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorHero.CleanArchitecture.Client.Pages.CashPower
 {
     public partial class Pos
     {
+        private MakeAPaymentCommand model;
+        private Decimal Amount;
+        private string SerialNumber;
+
+
+        public Task SubmitPayement()
+        {
+            return Task.CompletedTask;
+        }
     }
 }
---
+++ src/Client/Shared/Components/MoneyField.cs
@@ -0,0 +1,30 @@
+﻿using BlazorHero.CleanArchitecture.Client.Pages.Identity;
+using MudBlazor;
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace BlazorHero.CleanArchitecture.Client.Shared.Components;
+
+public class MoneyTextField<T> : MudTextField<T>
+{
+    public MoneyTextField()
+    {
+        Margin = Margin.Dense;
+        Format = "N0";
+        Adornment = Adornment.End;
+        AdornmentText = "FCFA";
+        Variant = Variant.Outlined;
+        Culture = CultureInfo.GetCultureInfo("de-DE");
+
+    }
+}
+public class MoneyField : MudField
+{
+    public MoneyField()
+    {
+        Margin = Margin.Dense;
+        Adornment = Adornment.End;
+        AdornmentText = "FCFA";
+        Variant = Variant.Text;
+    }
+}
