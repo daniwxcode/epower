f6e64661a734d7a2a291c005beb475ce03e4a14f
Daniel WOAGOU <daniwx@gmail.com>
2023-11-07 11:36:14 +0000

    ajout du champ de facturation

---
+++ src/Infrastructure/Migrations/20231107113410_BilledValues.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorHero.CleanArchitecture.Infrastructure.Migrations
+{
+    public partial class BilledValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "BilledAmount",
+                table: "Payments",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BilledAmount",
+                table: "Payments");
+        }
+    }
+}
